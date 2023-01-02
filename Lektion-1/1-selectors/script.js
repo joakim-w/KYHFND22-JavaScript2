931d21ca163082da6ff52c97c4375791b20650a4
Joakim <wahlstrom.joakim@gmail.com>
2023-01-02 09:53:32 +0100

    lektion 1

---
+++ Lektion-1/1-selectors/script.js
@@ -0,0 +1,23 @@
+
+
+// const header = document.getElementById('main-header')
+// const headings = document.getElementsByTagName('h2')
+// const listItems = document.getElementsByClassName('list-item')
+
+
+// console.log(headings)
+
+//Query Selector
+
+const header = document.querySelector('#main-header')
+
+const ptag = document.querySelector('.list-item:nth-child(2) span')
+
+const listItems = document.querySelectorAll('.list-item')
+
+console.log(ptag)
+
+// listItems.forEach(item => {
+//   console.log(item)
+// })
+
