2c5b9e23ec95574ca34f959bb5ce8d7038b3dea2
joakim w <joakim.wahlstrom@epndata.com>
2023-01-04 14:24:09 +0100

    lektion 2

--- Lektion-2/2-accordion/script.js
+++ Lektion-2/2-accordion/script.js
@@ -1,7 +1,19 @@
 const questions = document.querySelectorAll('.question')
+const wrapper = document.querySelector('.wrapper')
+
 
 questions.forEach(question => {
   question.addEventListener('click', e => {
     question.parentElement.classList.toggle('hidden')
   })
+})
+
+
+wrapper.addEventListener('click', e => {
+
+  if(e.target == wrapper) {
+    questions.forEach(question => {
+      question.parentElement.classList.add('hidden')
+    })
+  }
 })
