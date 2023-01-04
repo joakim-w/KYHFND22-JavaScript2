c1f2997404043c0d208fb6b97f6bf13d26764807
joakim w <joakim.wahlstrom@epndata.com>
2023-01-04 12:05:16 +0100

    lektion 2

--- Lektion-2/1-repetition/script.js
+++ Lektion-2/1-repetition/script.js
@@ -1,5 +1,23 @@
 
 const form = document.querySelector('.card form');
+const output = document.querySelector('#output')
+output.innerHTML = ''
+
+let items = [
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: 'Mjölk',
+  //   complete: true
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: 'Bröd',
+  //   complete: false
+  // },
+]
+
+
+
 
 // OM vi vill kunna ta bort en eventListener så "måste" vi skriva funktionen utanför
 // const test = (e) => {
@@ -19,13 +37,33 @@ const form = document.querySelector('.card form');
 document.querySelector('#output').addEventListener('click', e => {
   if(e.target.innerText === 'delete'){
     e.target.parentElement.remove()
+    items = items.filter(item => item.id !== e.target.id)
+    localStorage.setItem('itemList', JSON.stringify(items))
   }
   if(e.target.innerText === 'edit'){
 
   }
+
+  if(e.target.nodeName === 'P') {
+    e.target.style.textDecoration = 'line-through'
+  }
+  if(e.target.nodeName === 'DIV') {
+    // e.target.style.textDecoration = 'line-through'
+    // console.log(e.target.firstElementChild)
+    e.target.querySelector('p').classList.toggle('completed')
+  }
 })
 
 const createItemElement = (inputValue) => {
+  newItem = {
+    id: crypto.randomUUID(),
+    name: inputValue,
+    complete: false
+  }
+
+  items.push(newItem)
+  
+
 
   const item = document.createElement('div')
   item.classList.add('item')
@@ -35,6 +73,7 @@ const createItemElement = (inputValue) => {
 
   const button = document.createElement('button')
   button.innerText = 'delete'
+  button.id = newItem.id
 
   // button.addEventListener('click', () => {
   //   button.parentElement.remove()
@@ -46,6 +85,9 @@ const createItemElement = (inputValue) => {
 
 
   // document.querySelector('#output').appendChild(item)
+
+
+
   return item
 }
 
@@ -53,14 +95,37 @@ const createItemElement = (inputValue) => {
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
   const input = form.querySelector('input[type=text]');
   const inputValue = input.value;
 
+  if(inputValue.trim() === '') {
+    return
+  }
+
+
   const item = createItemElement(inputValue);
   document.querySelector('#output').appendChild(item);
+  localStorage.setItem('itemList', JSON.stringify(items))
+
 
   // input.value = '';
   form.reset()
 
 })
+
+
+
+const loadItems = () => {
+  const storedItems = JSON.parse(localStorage.getItem('itemList'))
+  
+  items = []
+  if(storedItems) {
+    items = storedItems
+  }
+  
+  items.forEach(item => {
+    output.appendChild(createItemElement(item.name));
+  })
+}
+
+loadItems()
