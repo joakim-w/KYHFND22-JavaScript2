d7286bf2bdc470d64975652b0262862fa674687c
joakim w <joakim.wahlstrom@epndata.com>
2023-01-11 10:52:08 +0100

    lektion 4

--- Lektion-4/övningsuppgift-3/script.js
+++ Lektion-4/övningsuppgift-3/script.js
@@ -3,18 +3,30 @@ const users = []
 
 const userList = document.querySelector('#user-list')
 
-const getUsers = () => {
-  fetch(BASE_URL)
-    .then(res => res.json())
-    .then(data => {
+// const getUsers = () => {
+//   fetch(BASE_URL)
+//   .then(res => res.json())
+//   .then(data => {
+    
+//     console.log(data)
+//     data.forEach(user => {
+//       users.push(user)
+//     });
+    
+//     listUsers()
+    
+//   })
+// }
 
-      data.forEach(user => {
-        users.push(user)
-      });
+const getUsers = async () => {
+  const res = await fetch(BASE_URL)
+  const data = await res.json()
 
-      listUsers()
-      
-    })
+  // console.log(data)
+  data.forEach(user => {
+    users.push(user)
+  })
+  listUsers()
 }
 
 
@@ -22,26 +34,47 @@ getUsers()
 // console.log(users)
 
 const listUsers = () => {
+  userList.innerHTML = ''
+
   users.forEach(user => {
-    userList.appendChild()
+    
+    const userElement = createUserElement(user)
+    userList.appendChild(userElement)
   })
 }
 
 
-const createUserElement = () => {
+const createUserElement = (userData) => {
   let user = document.createElement('div')
   user.classList.add('user')
   user.classList.add('user-dark')
 
   let name = document.createElement('p')
   name.classList.add('user_name')
+  name.innerText = userData.name
   
   let company = document.createElement('p')
   company.classList.add('user_company')
+  company.innerText = userData.company.name
 
   let email = document.createElement('p')
   email.classList.add('user_email')
+  email.innerText = userData.email
+
+  user.appendChild(name)
+  user.appendChild(company)
+  user.appendChild(email)
+
+  return user
+}
+
+const removeUser = e => {
+  if(!e.target.classList.contains('user')){
+    console.log('klickade inte på en div')
+    return
+  }
 
   
-
 }
+
+userList.addEventListener('click', removeUser)
